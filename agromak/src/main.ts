879c459ac4f5fc510ee8b605df5ac80aa97de67c
petar-stojanovic <2001petarstojanovic@gmail.com>
2024-01-28 20:40:14 +0100

    added firestore

--- agromak/src/main.ts
+++ agromak/src/main.ts
@@ -1,4 +1,4 @@
-import {enableProdMode} from '@angular/core';
+import {enableProdMode, importProvidersFrom} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideRouter, RouteReuseStrategy} from '@angular/router';
 import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
@@ -6,6 +6,10 @@ import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone
 import {routes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
 import {environment} from './environments/environment';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getDatabase, provideDatabase} from '@angular/fire/database';
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +19,15 @@ bootstrapApplication(AppComponent, {
   providers: [
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId": "agromak-1e9c8",
+      "appId": "1:585229624701:web:be931deaeab5e8bbab9814",
+      "storageBucket": "agromak-1e9c8.appspot.com",
+      "apiKey": "AIzaSyCYCO6jVkJ9vKj6RY-2TOuingNBBJfI2a8",
+      "authDomain": "agromak-1e9c8.firebaseapp.com",
+      "messagingSenderId": "585229624701",
+      "measurementId": "G-F9MMC2YPZD"
+    }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())),
+
   ],
 });
