ca29e1f0790f8f136785e8a5d071eefae77c7855
petar-stojanovic <2001petarstojanovic@gmail.com>
2024-01-30 21:13:00 +0100

    changed login page

--- agromak/src/app/pages/auth/login/login.page.ts
+++ agromak/src/app/pages/auth/login/login.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
+import {addIcons} from "ionicons";
+import {lockClosed, personOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-login',
@@ -10,40 +12,57 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class LoginPage  {
+export class LoginPage {
+
+  formErrorMessages = {
+    email: [
+      {type: "required", message: "Please enter your Email"},
+      {type: "pattern", message: "The Email entered is Incorrect"}
+    ],
+    password: [
+      {type: "required", message: "Please Enter your Password"},
+      {type: "minlength", message: "The Password must be at least 8 characters or more"}
+    ]
+  }
 
   screen: any = 'signin';
   formData: FormGroup;
   isLoading: boolean = false;
-  constructor(private fb:FormBuilder) {
+
+  constructor(private fb: FormBuilder) {
+
+    addIcons({personOutline,lockClosed})
+
     this.formData = this.fb.group({
-      name: ['',[Validators.required]],
-      email: ['',[Validators.required, Validators.email]],
-      password: ['',[Validators.required]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
 
 
-  change(event: any){
+  change(event: any) {
     this.screen = event;
   }
 
-  login(){
-    var formData: any = new FormData();
-    if(this.formData.valid){
+  login() {
+    const formData: any = new FormData();
+    console.log(this.formData)
+    if (this.formData.valid) {
       this.isLoading = true
       formData.append('email', this.formData.get('email')?.value);
       formData.append('password', this.formData.get('password')?.value);
       console.log(this.formData)
+
       // this.auth.userLogin(formData).subscribe((data:any)=>{
       //   console.log(data);
       // });
     }
   }
 
-  register(){
-    var formData: any = new FormData();
-    if(this.formData.valid){
+  register() {
+    const formData: any = new FormData();
+    if (this.formData.valid) {
       this.isLoading = true
       formData.append('name', this.formData.get('name')?.value);
       formData.append('email', this.formData.get('email')?.value);
--- agromak/src/environments/environment.prod.ts
+++ agromak/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCYCO6jVkJ9vKj6RY-2TOuingNBBJfI2a8",
+    authDomain: "agromak-1e9c8.firebaseapp.com",
+    projectId: "agromak-1e9c8",
+    storageBucket: "agromak-1e9c8.appspot.com",
+    messagingSenderId: "585229624701",
+    appId: "1:585229624701:web:be931deaeab5e8bbab9814",
+    measurementId: "G-F9MMC2YPZD"
+  }
 };
