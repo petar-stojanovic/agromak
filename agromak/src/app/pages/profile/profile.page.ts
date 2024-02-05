011960c295df32b687fd73232d61aab5c0c1f0c8
petar-stojanovic <2001petarstojanovic@gmail.com>
2024-02-05 22:42:53 +0100

    added upload profile photo functionality

--- agromak/src/app/pages/profile/profile.page.ts
+++ agromak/src/app/pages/profile/profile.page.ts
@@ -1,24 +1,48 @@
 import {Component, inject} from '@angular/core';
-import {IonButton, IonContent, IonHeader, IonIcon, IonItem, IonTitle, IonToolbar, LoadingController} from '@ionic/angular/standalone';
+import {
+  AlertController,
+  IonAvatar,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonText,
+  IonTitle,
+  IonToolbar,
+  LoadingController
+} from '@ionic/angular/standalone';
 import {addIcons} from "ionicons";
 import {logOutOutline} from "ionicons/icons";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
+import {ImageService} from "../../services/image.service";
+import {NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {Camera, CameraResultType, CameraSource} from "@capacitor/camera";
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.page.html',
   styleUrls: ['profile.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonButton, IonIcon],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonButton, IonIcon, IonAvatar, NgIf, IonText, FormsModule],
 })
 export class ProfilePage {
   private _authService = inject(AuthService);
 
-  constructor(private router: Router,
-              private loadingController: LoadingController) {
+  profile: any = null;
 
+  constructor(private router: Router,
+              private loadingController: LoadingController,
+              private imageService: ImageService,
+              private alertController: AlertController) {
     addIcons({logOutOutline})
+    this.imageService.getUserProfile().subscribe((data) => {
+      this.profile = data;
+      console.log(data)
+    })
+
   }
 
   async logOut() {
@@ -28,5 +52,32 @@ export class ProfilePage {
     this._authService.signOut().finally(() => {
       loading.dismiss();
     });
+  }
+
+  async changeImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Photos
+    });
+
+    console.log(image)
+
+    if (image) {
+      const loading = await this.loadingController.create();
+      await loading.present();
+
+      const result = await this.imageService.uploadImage(image);
+
+      if (!result) {
+        const alert = await this.alertController.create({
+          header: 'Upload Failed',
+          message: 'There was an error uploading your image',
+          buttons: ['OK']
+        });
+      }
+      await loading.dismiss();
+    }
   }
 }
--- agromak/src/app/services/image.service.ts
+++ agromak/src/app/services/image.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {getDownloadURL, ref, Storage, uploadString} from "@angular/fire/storage";
+import {AuthService} from "./auth.service";
+import {doc, docData, Firestore, setDoc} from "@angular/fire/firestore";
+import {Auth} from "@angular/fire/auth";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {User} from "../interfaces/user";
+import {Photo} from "@capacitor/camera";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  constructor(private auth: Auth,
+              private firestore: Firestore,
+              private angularFirestore: AngularFirestore,
+              private afAuth: AngularFireAuth,
+              private authService: AuthService,
+              private storage: Storage) {
+    // console.log(this.auth.currentUser)
+    // this.afAuth.currentUser.then(value => console.log(value))
+    // authService.user$.subscribe(value => console.log(value))
+    // console.log(this.getUserProfile())
+  }
+
+
+  getUserProfile() {
+    const user = this.auth.currentUser as User;
+    const userDocRef = doc(this.firestore, `users/${user.uid}`)
+    return docData(userDocRef);
+  }
+
+  async uploadImage(cameraFile: Photo) {
+    const user = this.auth.currentUser as User;
+    const path = `uploads/${user.uid}/profile.png`;
+    const storageRef = ref(this.storage, path);
+
+    try {
+      await uploadString(storageRef, cameraFile.base64String!, 'base64')
+
+      const imageUrl = await getDownloadURL(storageRef);
+
+      const userDocRef = doc(this.firestore, `users/${user.uid}`)
+      await setDoc(userDocRef, {
+          photoURL: imageUrl
+        }, {
+          merge: true
+        }
+      );
+      return true;
+
+    } catch (e) {
+      console.log("ERROR- ", e)
+      return null;
+    }
+  }
+
 }
