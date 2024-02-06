c942b0b03dd2957bd3a0e36b1830627583b09666
petar-stojanovic <2001petarstojanovic@gmail.com>
2024-02-06 16:44:49 +0100

    changed style and validators

--- agromak/src/app/pages/auth/register/register.page.ts
+++ agromak/src/app/pages/auth/register/register.page.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {
   AlertController,
   IonButton,
@@ -16,7 +16,7 @@ import {
 } from "@ionic/angular/standalone";
 import {Router, RouterLink} from "@angular/router";
 import {addIcons} from "ionicons";
-import {eye, eyeOff, lockClosed, logoGoogle, personOutline} from "ionicons/icons";
+import {eye, eyeOff, lockClosed, logoGoogle, mail, person} from "ionicons/icons";
 import {AuthService} from "../../../services/auth.service";
 import firebase from "firebase/compat";
 import {ShowHidePasswordComponent} from "../show-hide-password/show-hide-password.component";
@@ -38,7 +38,11 @@ export class RegisterPage {
     ],
     password: [
       {type: "required", message: "Please enter your Password"},
-      {type: "minlength", message: "The Password must be at least 8 characters"}
+      {type: "minlength", message: "The Password must be at least 8 characters"},
+      {type: "passwordsDontMatchError", message: "Passwords must match"}
+    ],
+    confirmPassword: [
+      {type: "passwordsDontMatchError", message: "Passwords must match"},
     ]
   }
 
@@ -46,6 +50,8 @@ export class RegisterPage {
 
   form!: FormGroup;
   screen: string = 'login';
+  showPassword = false;
+  showConfirmPassword = false;
 
 
   constructor(private fb: FormBuilder,
@@ -53,28 +59,33 @@ export class RegisterPage {
               private alertController: AlertController,
               private router: Router) {
 
-    addIcons({personOutline, lockClosed, logoGoogle, eye, eyeOff})
+    addIcons({person, lockClosed, mail, logoGoogle, eye, eyeOff})
     this.initForm();
   }
 
   initForm() {
     this.form = this.fb.group({
+        // name: ['Test Testing', [Validators.required]],
         // email: ['test@test.com', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]],
         // password: ['Test123!', [Validators.required, Validators.minLength(8)]],
-        // confirmPassword: ['Test123!', Validators.required],
+        // confirmPassword: ['Test123!', [Validators.required, matchPasswordValidator()]],
+        name: ['', [Validators.required]],
         email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]],
         password: ['', [Validators.required, Validators.minLength(8)]],
-        confirmPassword: ['', Validators.required]
+        confirmPassword: ['', [Validators.required]]
       },
       {
         validators: this.passwordMatchValidator
       });
 
+    this.form.valueChanges.subscribe(() => {
+      console.log(this.form.errors)
+    })
   }
 
-  passwordMatchValidator(control: AbstractControl) {
-    const password = control.get("password")?.value;
-    const confirmPassword = control.get("confirmPassword")?.value;
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get("password")?.value;
+    const confirmPassword = form.get("confirmPassword")?.value;
     return password === confirmPassword ? null : {mismatch: true};
   }
 
@@ -86,8 +97,16 @@ export class RegisterPage {
     return this.form.get('password')!;
   }
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
   get confirmPassword() {
     return this.form.get('confirmPassword')!;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
   }
 
   async register() {
