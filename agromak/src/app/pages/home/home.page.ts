6c48ded3c8cf4b83c9e8c832ce706ecb4f9c12e2
petar-stojanovic <banakafala1a@gmail.com>
2024-02-19 14:22:33 +0100

    changed visual effectr and added placeholder

--- agromak/src/app/pages/home/home.page.ts
+++ agromak/src/app/pages/home/home.page.ts
@@ -1,33 +1,58 @@
+import {Haptics, ImpactStyle} from '@capacitor/haptics';
 import {Component} from '@angular/core';
 import {
   IonButton,
   IonContent,
   IonHeader,
-  IonIcon, IonItem, IonLabel, IonList,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader, IonRefresher, IonRefresherContent, IonSkeletonText,
+  IonText,
+  IonThumbnail,
   IonTitle,
   IonToolbar,
   ModalController
 } from '@ionic/angular/standalone';
 import {AddProductModalComponent} from "../../components/add-product-modal/add-product-modal.component";
 import {AdService} from "../../services/ad.service";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {addIcons} from "ionicons";
+import {RefresherCustomEvent} from "@ionic/angular";
+import {addWarning} from "@angular-devkit/build-angular/src/utils/webpack-diagnostics";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonList, IonItem, NgForOf, IonLabel],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonList, IonItem, NgForOf, IonLabel, IonListHeader, IonText, IonThumbnail, NgIf, IonRefresher, IonRefresherContent, IonSkeletonText],
 })
 export class HomePage {
 
   ads: any;
+
+  placeholderArray = new Array(5);
+
   constructor(private modalCtrl: ModalController,
               private _adService: AdService) {
+    addIcons({})
     // this.openModal();
+    this.getAllAds();
+  }
+
+  async getAllAds(event?: RefresherCustomEvent) {
+    if (event) {
+      await Haptics.impact({style: ImpactStyle.Medium});
+      this.ads = [];
+    }
     this._adService.getAllAds().subscribe((ads) => {
       this.ads = ads.docs.map((ad) => ad.data());
-      console.log(this.ads);
+      console.log(ads)
+      if (event) {
+        event.target.complete();
+      }
     });
   }
 
@@ -44,4 +69,16 @@ export class HomePage {
     }
   }
 
+  async handleRefresh(event: RefresherCustomEvent) {
+    await Haptics.impact({style: ImpactStyle.Medium});
+    this.ads = [];
+
+    this.getAllAds();
+
+    setTimeout(() => {
+      // Any calls to load data go here
+      this.getAllAds();
+      event.target.complete();
+    }, 2000);
+  }
 }
