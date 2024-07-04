ea5906de4f0f35c320e464b9fcd31dba9bb23444
petar-stojanovic <banakafala1a@gmail.com>
2024-07-04 13:07:03 +0200

    create AdListComponent

---
+++ agromak/src/app/components/ad-list/ad-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-list',
+  templateUrl: './ad-list.component.html',
+  styleUrls: ['./ad-list.component.scss'],
+})
+export class AdListComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
