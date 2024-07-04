c24313bc1014c65b219340e7b7e31749d4c08b25
petar-stojanovic <banakafala1a@gmail.com>
2024-07-04 14:18:24 +0200

    create AddDeatilsModal Component

---
+++ agromak/src/app/components/ad-details-modal/ad-details-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-details-modal',
+  templateUrl: './ad-details-modal.component.html',
+  styleUrls: ['./ad-details-modal.component.scss'],
+})
+export class AdDetailsModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
