7e98b6d862e153f0d4c52a0f7fc5abe6daaaa302
petar-stojanovic <banakafala1a@gmail.com>
2024-07-10 13:21:12 +0200

    created SearchAdsModalComponent

---
+++ agromak/src/app/components/search-ads-modal/search-ads-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-ads-modal',
+  templateUrl: './search-ads-modal.component.html',
+  styleUrls: ['./search-ads-modal.component.scss'],
+  standalone: true
+})
+export class SearchAdsModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
