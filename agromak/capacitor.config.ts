e375e803208d87063603e928fb2e0068267fe4a4
petar-stojanovic <banakafala1a@gmail.com>
2024-07-12 12:35:30 +0200

    changed launchShowDuration from 2500ms to 1500ms

--- agromak/capacitor.config.ts
+++ agromak/capacitor.config.ts
@@ -11,7 +11,7 @@ const config: CapacitorConfig = {
       "forceCodeForRefreshToken": true
     },
     SplashScreen: {
-      launchShowDuration: 2500,
+      launchShowDuration: 1500,
       launchAutoHide: true,
     }
   }
