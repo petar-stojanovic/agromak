4f874f87cf1856a28f0b4e7776f8d38225ba7507
petar-stojanovic <banakafala1a@gmail.com>
2024-07-12 13:28:12 +0200

    optimized error message DOM loading in global error component

--- agromak/src/app/components/input-error/input-error.component.ts
+++ agromak/src/app/components/input-error/input-error.component.ts
@@ -1,15 +1,17 @@
 import {ChangeDetectionStrategy, Component, HostBinding, inject, Input, OnInit} from '@angular/core';
 import {ValidationErrors} from "@angular/forms";
-import {KeyValuePipe, NgForOf} from "@angular/common";
+import {KeyValue, KeyValuePipe, NgForOf} from "@angular/common";
 import {VALIDATION_ERROR_MESSAGES} from "./validation-error-messages.token";
 import {IonNote} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-input-error',
   template: `
-    <ion-note *ngFor="let error of errors | keyvalue " class="input-error">
-      {{ errorsMap[error.key](error.value) }}
-    </ion-note>
+    @for (error of errors | keyvalue; track error.key) {
+      <ion-note class="input-error">
+        {{ errorsMap[error.key](error.value) }}
+      </ion-note>
+    }
   `,
   styles: `
     :host {
@@ -32,9 +34,5 @@ export class InputErrorComponent {
   @HostBinding('attr.slot') slot = 'error'
 
   protected errorsMap = inject(VALIDATION_ERROR_MESSAGES)
-
-  constructor() {
-  }
-
 
 }
