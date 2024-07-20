7fc22e9826674cb2b600adc27ce03a5ba8b2d8f1
petar-stojanovic <banakafala1a@gmail.com>
2024-07-20 05:24:33 +0200

    added additionalFields to CreateDynamicAd interface

--- agromak/src/app/shared/models/create-dynamic-ad-.ts
+++ agromak/src/app/shared/models/create-dynamic-ad-.ts
@@ -1,15 +1,17 @@
 import {GalleryPhoto} from "@capacitor/camera";
 
 export interface CreateDynamicAd {
-  category: string
-  itemCondition: string
-  title: string
-  title_lowercase: string
-  description: string
-  price: string
-  currency: string
-  fixedPrice: boolean
-  location: string
-  phone: string
-  images?: GalleryPhoto[]
+  category: string;
+  itemCondition: string;
+  title: string;
+  title_lowercase: string;
+  description: string;
+  price: string;
+  currency: string;
+  fixedPrice: boolean;
+  location: string;
+  phone: string;
+  images?: GalleryPhoto[];
+  additionalFields?: [{name: string, value: string}];
 }
+
