0a42bf9b496d9830e2af1d3f61d5847317d2feb8
petar-stojanovic <banakafala1a@gmail.com>
2024-07-23 17:24:46 +0200

    improved skeleton for profile page

--- agromak/src/app/pages/profile/favorite-ads/favorite-ads.component.ts
+++ agromak/src/app/pages/profile/favorite-ads/favorite-ads.component.ts
@@ -1,14 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  ModalController
+} from "@ionic/angular/standalone";
+import {AdListComponent} from "../../../components/ad-list/ad-list.component";
+import {Ad} from "../../../shared/models/ad";
+import {AdService} from "../../../services/ad.service";
 
 @Component({
   selector: 'app-favorite-ads',
   templateUrl: './favorite-ads.component.html',
   styleUrls: ['./favorite-ads.component.scss'],
+  imports: [
+    AdListComponent,
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar
+  ],
+  standalone: true
 })
-export class FavoriteAdsComponent  implements OnInit {
+export class FavoriteAdsComponent   implements OnInit {
 
-  constructor() { }
+  ads: Ad[] = [];
+  isLoading = true;
 
-  ngOnInit() {}
+  constructor(private modalCtrl: ModalController,
+              private adService: AdService) { }
 
+  ngOnInit() {
+
+  }
+
+
+  dismiss() {
+    return this.modalCtrl.dismiss();
+  }
 }
--- agromak/src/app/pages/profile/my-ads/my-ads.component.ts
+++ agromak/src/app/pages/profile/my-ads/my-ads.component.ts
@@ -1,14 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  ModalController
+} from "@ionic/angular/standalone";
+import {AdListComponent} from "../../../components/ad-list/ad-list.component";
+import {Ad} from "../../../shared/models/ad";
+import {AdService} from "../../../services/ad.service";
 
 @Component({
   selector: 'app-my-ads',
   templateUrl: './my-ads.component.html',
   styleUrls: ['./my-ads.component.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    AdListComponent
+  ],
+  standalone: true
 })
 export class MyAdsComponent  implements OnInit {
 
-  constructor() { }
+  ads: Ad[] = [];
+  isLoading = true;
 
-  ngOnInit() {}
+  constructor(private modalCtrl: ModalController,
+              private adService: AdService) { }
 
+  ngOnInit() {
+
+  }
+
+
+  dismiss() {
+    return this.modalCtrl.dismiss();
+  }
 }
