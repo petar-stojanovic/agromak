1e5fd3f1ad5a75cbbd6fe2027e08fea1333c0247
petar-stojanovic <banakafala1a@gmail.com>
2024-07-23 17:09:31 +0200

    created my-ads and favorite-ads components in profile page

---
+++ agromak/src/app/pages/profile/favorite-ads/favorite-ads.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-ads',
+  templateUrl: './favorite-ads.component.html',
+  styleUrls: ['./favorite-ads.component.scss'],
+})
+export class FavoriteAdsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
