4b67fb65433e241b40d886b27dcb27f101bada6a
petar-stojanovic <banakafala1a@gmail.com>
2024-07-29 17:46:41 +0200

    added styling for profile photo in edit page

--- agromak/src/app/pages/profile/edit-profile/edit-profile.page.ts
+++ agromak/src/app/pages/profile/edit-profile/edit-profile.page.ts
@@ -1,7 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput, IonItem, IonLabel, IonList, IonText, IonThumbnail,
+  IonTitle,
+  IonToolbar
+} from "@ionic/angular/standalone";
 import {AuthService} from "../../../services/auth.service";
 
 @Component({
@@ -9,16 +17,37 @@ import {AuthService} from "../../../services/auth.service";
   templateUrl: './edit-profile.page.html',
   styleUrls: ['./edit-profile.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton]
+  imports: [CommonModule, FormsModule, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, ReactiveFormsModule, IonInput, IonItem, IonLabel, IonText, IonThumbnail, IonList]
 })
 export class EditProfilePage implements OnInit {
 
   user$ = this.authService.user$;
+  form!: FormGroup;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private fb: FormBuilder) {
   }
 
   ngOnInit() {
+    this.createForm();
+    this.authService.user$.subscribe(user => {
+      console.log(user);
+    })
   }
 
+  private createForm() {
+    this.form = this.fb.group({
+      displayName: [''],
+      email: [''],
+      phoneNumber: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  changeImage() {
+
+  }
 }
