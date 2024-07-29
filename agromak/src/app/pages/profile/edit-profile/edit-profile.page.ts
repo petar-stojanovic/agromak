1874c0c35d391fb8797c634c2828ad262af836cb
petar-stojanovic <banakafala1a@gmail.com>
2024-07-29 15:23:37 +0200

    added back button to EditProfilePage

--- agromak/src/app/pages/profile/edit-profile/edit-profile.page.ts
+++ agromak/src/app/pages/profile/edit-profile/edit-profile.page.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.page.html',
   styleUrls: ['./edit-profile.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton]
 })
 export class EditProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit() {
   }
