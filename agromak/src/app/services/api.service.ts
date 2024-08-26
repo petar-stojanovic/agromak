7e5212d495e8f15fb6458a6a8da35c7dcb6e00af
petar-stojanovic <banakafala1a@gmail.com>
2024-08-26 13:07:41 +0200

    added general firebase requests to ApiService

--- agromak/src/app/services/api.service.ts
+++ agromak/src/app/services/api.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {collection, collectionData, doc, Firestore, getDoc, query, setDoc, where} from "@angular/fire/firestore";
+import firebase from "firebase/compat";
+import WhereFilterOp = firebase.firestore.WhereFilterOp;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) {
+  }
+
+
+  docRef(path: string) {
+    return doc(this.firestore, path);
+  }
+
+  collectionRef(path: string) {
+    return collection(this.firestore, path);
+  }
+
+  setDocument(path: string, data: any) {
+    const dataRef = this.docRef(path);
+    return setDoc(dataRef, data);
+  }
+
+  getDocById(path: string) {
+    const dataRef = this.docRef(path);
+    return getDoc(dataRef);
+  }
+
+  collectionDataQuery(path: string, queryFn?: any) {
+    let dataRef: any = this.collectionRef(path);
+    if (queryFn) {
+      dataRef = query(dataRef, queryFn);
+    }
+    return collectionData<any>(dataRef);
+  }
+
+  whereQuery(fieldPath: string, condition: WhereFilterOp, value: any) {
+    return where(fieldPath, condition, value);
+  }
 }
