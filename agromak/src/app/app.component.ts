db480f90b33c620b62716429ea2060d7a5c6a1e3
petar-stojanovic <banakafala1a@gmail.com>
2024-08-27 18:56:41 +0200

    added push notification when the app starts

--- agromak/src/app/app.component.ts
+++ agromak/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {IonApp, IonRouterOutlet, Platform} from '@ionic/angular/standalone';
 import {register} from 'swiper/element/bundle';
+import {ActionPerformed, PushNotifications, PushNotificationSchema, Token} from "@capacitor/push-notifications";
 
 register();
 
@@ -13,7 +14,7 @@ register();
     IonRouterOutlet
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @ViewChild(IonRouterOutlet) outlet!: IonRouterOutlet;
 
@@ -23,5 +24,49 @@ export class AppComponent {
         // App.exitApp();
       }
     });
+  }
+
+  ngOnInit() {
+    console.log('Initializing HomePage');
+
+    PushNotifications.requestPermissions().then(result => {
+
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+        alert('Push notifications are not enabled');
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
+
   }
 }
