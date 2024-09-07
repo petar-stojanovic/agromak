b73e8b10ff6dcd7754f70afc2100b98885769806
petar-stojanovic <banakafala1a@gmail.com>
2024-09-07 17:41:19 +0200

    refactored HideHeaderDirective

--- agromak/src/app/shared/directives/hide-header.directive.ts
+++ agromak/src/app/shared/directives/hide-header.directive.ts
@@ -9,7 +9,7 @@ export class HideHeaderDirective implements OnInit {
 
   @Input('appHideHeader') header: any;
   private headerHeight = 44;
-  private scrollTop = 44;
+  private lastScrollTop = 44;
   private SCROLL_THRESHOLD = 10;
 
   constructor(private renderer: Renderer2,
@@ -20,26 +20,22 @@ export class HideHeaderDirective implements OnInit {
     this.header = this.header.el;
     this.domCtrl.read(() => {
       this.headerHeight = this.header.clientHeight;
-      this.scrollTop = this.header.clientHeight;
+      this.lastScrollTop = this.header.clientHeight;
     });
   }
 
-
   @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
-    console.log($event);
     const scrollTop = $event.detail.scrollTop;
 
-    if (Math.abs(scrollTop - this.scrollTop) > this.SCROLL_THRESHOLD && scrollTop > this.headerHeight) {
-
-      if (scrollTop > this.scrollTop) {
+    if (Math.abs(scrollTop - this.lastScrollTop) > this.SCROLL_THRESHOLD && scrollTop > this.headerHeight) {
+      if (scrollTop > this.lastScrollTop) {
         this.renderer.setStyle(this.header, 'top', `-${this.headerHeight}px`);
       } else {
         this.renderer.setStyle(this.header, 'top', '0');
       }
 
       this.renderer.setStyle(this.header, 'transition', `top 400ms`);
-      this.scrollTop = scrollTop;
+      this.lastScrollTop = scrollTop;
     }
   }
-
 }
